37a5f305781f9670b2c196ed0503590e392f8bc1
gjfLeo <gjfLeo@yeah.net>
2019-06-02 20:51:48 +0800

    Update time.js
    日期加0

--- WebRoot/js/time.js
+++ WebRoot/js/time.js
@@ -2,13 +2,16 @@
  * 
  */
 function time() {
-	var week = [ "星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" ];
+	var week = [ "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" ];
 	var d = new Date();
-	var date = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " "
+	var date = d.getFullYear() + "/"
+			+ (d.getMonth() < 9 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1))
+			+ "/" + (d.getDate() < 10 ? "0" + d.getDate() : d.getDate()) + " "
 			+ week[d.getDay()]
 	var time = (d.getHours() < 10 ? "0" + d.getHours() : d.getHours()) + ":"
 			+ (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes())
-			+ ":" + (d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds())
+			+ ":"
+			+ (d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds())
 	document.getElementById("time").innerHTML = date + "  " + time
 }
 
